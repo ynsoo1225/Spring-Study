86f7000ddfbfe40a4d8410f92caee1515aeca3c7
ala8343 <ala8343@gmail.com>
2021-02-17 23:32:22 +0900

    3,4강 실습

--- core/core/src/main/java/hello/core/MemberApp.java
+++ core/core/src/main/java/hello/core/MemberApp.java
@@ -2,12 +2,18 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     public static void main(String[] args) {
-        MemberService memberService = new MemberServiceImpl();
+//        AppConfig appConfig = new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
+
         Member member = new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
@@ -15,5 +21,4 @@ public class MemberApp {
         System.out.println("new member = " + member.getName());
         System.out.println("find Member = " + findMember.getName());
     }
-
 }
